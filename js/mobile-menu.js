8bdb29552ab1ae8b07d2a40172e329f655edf0d1
Volodymyr Tyshchenko <volodymyr.tyshchenko@gmail.com>
2021-06-19 21:28:16 +0300

    refactoring and bugfix header for mobile - mobile menu done

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,18 +1,35 @@
-(() => {
-    const menuBtnRef = document.querySelector('[data-menu-button]');
-    const mobileMenuRef = document.querySelector('[data-menu]');
-    const mobileMenuClose = document.querySelector('[data-menu-close]');
+// (() => {
+//     const menuBtnRef = document.querySelector('[data-menu-button]');
+//     const mobileMenuRef = document.querySelector('[data-menu]');
+//     const mobileMenuClose = document.querySelector('[data-menu-close]');
     
-    menuBtnRef.addEventListener("click", () => {
-        const extended =
-            menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-        menuBtnRef.classList.toggle("is-open");
-        menuBtnRef.setAttribute("aria-expanded", !extended);
+//     menuBtnRef.addEventListener("click", () => {
+//         const extended =
+//             menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+//         menuBtnRef.classList.toggle("is-open");
+//         menuBtnRef.setAttribute("aria-expanded", !extended);
 
-        mobileMenuRef.classList.toggle("is-open");
-    });
+//         mobileMenuRef.classList.toggle("is-open");
+//     });
   
-    mobileMenuClose.addEventListener("click", () => {
+    // document.body.classList.toggle("modal-open")
+//     mobileMenuClose.addEventListener("click", () => {
+//       mobileMenuRef.classList.toggle("is-open");
+//     });
+// })();
+(() => {
+    const menuBtnRef = document.querySelector("[data-menu-button]");
+    const mobileMenuRef = document.querySelector("[data-menu]");
+    const mobileBtnClose = document.querySelector("[data-menu-close]");
+  
+    menuBtnRef.addEventListener("click", () => {
+      document.body.classList.toggle("modal-open")
       mobileMenuRef.classList.toggle("is-open");
     });
-})();
+  
+  
+  mobileBtnClose.addEventListener("click", () => {
+      document.body.classList.toggle("modal-open")
+      mobileMenuRef.classList.toggle("is-open");
+    });
+  })();
